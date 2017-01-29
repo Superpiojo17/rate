213d9d788e4e0de06caf4e7d8a2d362181f735b1
Joel Sandoval <superpiojo17@gmail.com>
2017-01-28 20:26:58 -0600

    Updated routes on register and login controllers

--- src/main/java/edu/ben/rate_review/controller/home/LogInController.java
+++ src/main/java/edu/ben/rate_review/controller/home/LogInController.java
@@ -17,7 +17,7 @@ public class LogInController {
         // Just a hash to pass data from the servlet to the page
         HashMap<String, Object> model = new HashMap<>();
         // Tell the server to render the index page with the data in the model
-        return new ModelAndView(model, "home/login.hbs");
+        return new ModelAndView(model, "sessions/login.hbs");
     }
 
 
--- src/main/java/edu/ben/rate_review/controller/home/RegisterController.java
+++ src/main/java/edu/ben/rate_review/controller/home/RegisterController.java
@@ -14,7 +14,7 @@ public class RegisterController {
 		// Just a hash to pass data from the servlet to the page
 		HashMap<String, Object> model = new HashMap<>();
 		// Tell the server to render the index page with the data in the model
-		return new ModelAndView(model, "home/register.hbs");
+		return new ModelAndView(model, "sessions/register.hbs");
 	}
 
 }
