08f13c295d2182b81919615d62f21ed2f4fb0f87
Mike Webb <mwebb0162@gmail.com>
2017-03-01 20:27:42 -0600

    finished individual professor page besides formatting

--- src/main/java/edu/ben/rate_review/controller/home/ProfessorController.java
+++ src/main/java/edu/ben/rate_review/controller/home/ProfessorController.java
@@ -1,5 +1,6 @@
 package edu.ben.rate_review.controller.home;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class ProfessorController {
 	public ModelAndView showProfessorPage(Request req, Response res) {
 		// Just a hash to pass data from the servlet to the page
 		HashMap<String, Object> model = new HashMap<>();
-		
+
 		ProfessorReviewDao reviewDao = DaoManager.getInstance().getProfessorReviewDao();
 		Session session = req.session();
 		String idString = req.params("course_id");
@@ -26,38 +27,43 @@ public class ProfessorController {
 		List<ProfessorReview> reviews = reviewDao.professorByCourseId(id);
 		CoursesToReview course = reviewDao.findByCourseId(id);
 		// create the form object, put it into request
+		
 		double objectives = reviewDao.avgRateObjectives(course);
 		double organized = reviewDao.avgRateOrganized(course);
 		double challenging = reviewDao.avgRateChallenging(course);
-		double outside  = reviewDao.avgRateOutsideWork(course);
-		double pace  = reviewDao.avgRatePace(course);
+		double outside = reviewDao.avgRateOutsideWork(course);
+		double pace = reviewDao.avgRatePace(course);
 		double assignments = reviewDao.avgRateAssignments(course);
 		double grade_fairly = reviewDao.avgRateGradeFairly(course);
 		double grade_time = reviewDao.avgRateGradeTime(course);
 		double accessibility = reviewDao.avgRateAccessibility(course);
 		double knowledge = reviewDao.avgRateKnowledge(course);
 		double career = reviewDao.avgRateCareerDevelopment(course);
+		double overall = ((objectives + organized + challenging + outside + pace + assignments + grade_fairly
+				+ grade_time + accessibility + knowledge + career)/11);
+
+		DecimalFormat df = new DecimalFormat("0.0"); 
 		
-		model.put("rate_objective", objectives);
-		model.put("rate_organized", organized);
-		model.put("rate_challenging", challenging);
-		model.put("rate_outside_work", outside);
-		model.put("rate_pace", pace);
-		model.put("rate_assignments", assignments);
-		model.put("rate_grade_fairly", grade_fairly);
-		model.put("rate_grade_time", grade_time);
-		model.put("rate_accessibility", accessibility);
-		model.put("rate_knowledge", knowledge);
-		model.put("rate_career_development", career);
-		
-		
+		model.put("rate_objective", df.format(objectives));
+		model.put("rate_organized", df.format(organized));
+		model.put("rate_challenging", df.format(challenging));
+		model.put("rate_outside_work", df.format(outside));
+		model.put("rate_pace", df.format(pace));
+		model.put("rate_assignments", df.format(assignments));
+		model.put("rate_grade_fairly", df.format(grade_fairly));
+		model.put("rate_grade_time", df.format(grade_time));
+		model.put("rate_accessibility", df.format(accessibility));
+		model.put("rate_knowledge", df.format(knowledge));
+		model.put("rate_career_development", df.format(career));
+		model.put("overall", df.format(overall));
+
 		model.put("courses", reviews);
 		model.put("course", course);
-		
+
 		// Tell the server to render the index page with the data in the model
 		return new ModelAndView(model, "home/professor.hbs");
 	}
-	
+
 	public ModelAndView display(Request req, Response res) {
 		HashMap<String, Object> model = new HashMap<>();
 
