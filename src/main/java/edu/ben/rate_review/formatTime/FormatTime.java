661e838909179c78399e392839b3e6c9fccd404e
Mike Webb <mwebb0162@gmail.com>
2017-03-18 12:05:23 -0500

    fixed noon/midnight issue with time format

--- src/main/java/edu/ben/rate_review/formatTime/FormatTime.java
+++ src/main/java/edu/ben/rate_review/formatTime/FormatTime.java
@@ -22,10 +22,15 @@ public class FormatTime {
 		String amOrPm = "";
 		String minuteString = "";
 
-		if (hour > 12) {
-			hour = hour - 12;
+		if (hour >= 12) {
+			if (hour > 12){
+				hour = hour - 12;
+			}
 			amOrPm = " PM";
 		} else {
+			if (hour == 0){
+				hour = hour + 12;
+			}
 			amOrPm = " AM";
 		}
 
