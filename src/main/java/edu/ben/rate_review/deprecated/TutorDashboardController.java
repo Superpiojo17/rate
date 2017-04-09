46c0807849cd3803756137687832398eb058f5bc
Mike Webb <mwebb0162@gmail.com>
2017-04-09 16:17:04 -0500

    made changes to pages besides fixing menus and created deprecated folder

---
+++ src/main/java/edu/ben/rate_review/deprecated/TutorDashboardController.java
@@ -0,0 +1,18 @@
+//package edu.ben.rate_review.deprecated;
+//
+//import java.util.HashMap;
+//
+//import spark.ModelAndView;
+//import spark.Request;
+//import spark.Response;
+//
+//public class TutorDashboardController {
+//
+//	public ModelAndView showTutorDashboardPage(Request req, Response res) {
+//		// Just a hash to pass data from the servlet to the page
+//		HashMap<String, Object> model = new HashMap<>();
+//		// Tell the server to render the index page with the data in the model
+//		return new ModelAndView(model, "users/tutorDashboard.hbs");
+//	}
+//
+//}
