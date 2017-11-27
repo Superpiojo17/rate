c021fe4e36445c5bd94c76344623393f09e0ad5b
Joel Sandoval <superpiojo17@gmail.com>
2017-11-26 21:48:47 -0600

    test1

--- src/main/resources/public/assets/js/main.js
+++ src/main/resources/public/assets/js/main.js
@@ -1,24 +1,24 @@
 /*
-	Retrospect by TEMPLATED
+	Intensify by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
 (function($) {
 
-	skel
-		.breakpoints({
-			xlarge: '(max-width: 1680px)',
-			large: '(max-width: 1280px)',
-			medium: '(max-width: 980px)',
-			small: '(max-width: 736px)',
-			xsmall: '(max-width: 480px)'
-		});
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
 
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body');
+			$body = $('body'),
+			$header = $('#header');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -40,9 +40,16 @@
 				);
 			});
 
-		// Nav.
-			$('#nav')
-				.append('<a href="#nav" class="close"></a>')
+		// Scrolly.
+			$('.scrolly').scrolly({
+				offset: function() {
+					return $header.height();
+				}
+			});
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
 				.appendTo($body)
 				.panel({
 					delay: 500,
